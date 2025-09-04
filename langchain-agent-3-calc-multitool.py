0f0b3deebce887c63e92015d49e4f116e2afc1d6
chriswilder3 <chriswilderdelta19@gmail.com>
2025-09-04 21:15:23 +0530

    created multitool agent with simple calc and internet tools and langfuse tracing

---
+++ langchain-agent-3-calc-multitool.py
@@ -0,0 +1,61 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.tools import tool
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langfuse import Langfuse
+from langfuse.langchain import CallbackHandler
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+# Define the tools 
+@tool 
+def calculator(a, b, operator):
+    """ Performs addition, multiplication, subtraction, division depending on operator """
+    if operator == "+":
+        return a + b
+    elif operator == "*":
+        return a*b
+    elif operator == "-":
+        return a-b
+    elif operator == "/":
+        return a/b
+    
+searchtool = DuckDuckGoSearchRun()
+tools = [searchtool, calculator]
+
+langfuse = Langfuse(
+        public_key= os.getenv('LANGFUSE_PUBLIC_KEY'),
+        secret_key= os.getenv('LANGFUSE_SECRET_KEY')
+    )
+langfuse_handler = CallbackHandler()
+
+def invoke_agent():
+
+    llm = ChatOpenAI(
+        model = 'gpt-4.1-mini',
+        api_key = os.getenv('OPENAI_API_KEY'),
+        temperature = 0.4
+    )
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system" , "You are a helpful AI assistant. You can access internet to answer questions and use calculator tool to reply simple calculations. Incase the user asks to calculate based on current events, use tools again as many times needed to get the answer"),
+            ("user" , "{input}"),
+            ("placeholder" , "{agent_scratchpad}")
+        ]
+    )
+
+    agent = create_tool_calling_agent(llm, tools, prompt)
+    agent_executor = AgentExecutor(agent= agent, tools = tools, verbose= True)
+
+    user_input = input("Enter a topic/question : ")
+    print(" Agents's thinking strategy : ")
+    return agent_executor.invoke({"input":user_input}, config={"callbacks":[langfuse_handler]})
+
+response = invoke_agent()
+print("----LLM Response----")
+print(response['output'])
+
