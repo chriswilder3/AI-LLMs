40babd4b622f4a0f5a398e3e5109261fc8d6b493
chriswilder3 <chriswilderdelta19@gmail.com>
2025-09-12 01:12:29 +0530

    add better workflow, a notebook with graph diagram

---
+++ langgraph/langgraph-1-bmi_calc.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+from typing import TypedDict
+
+# BMI Calculator
+
+llm = ChatOpenAI(
+        model = 'gpt-4.1-mini',
+        api_key = os.getenv('OPENAI_API_KEY'),
+        temperature= 0.5,
+    )
+# State
+class BMIState(TypedDict):
+    height : float
+    weight : float
+    bmi : float
+
+# Runnable/functions (nodes)
+def calculate_bmi(state :BMIState) -> BMIState:
+    bmi = round(weight /  ((height)**2),2)
+    state["bmi"] = bmi
+    return state
+ 
+graph = StateGraph(state_schema= BMIState)
+
+# Add nodes by mapping name to python function/runnable
+graph.add_node('calculate_bmi',calculate_bmi) 
+        # we can also use: graph.add_sequence([calculate_bmi]) 
+
+# Add edges
+graph.add_edge(START, "calculate_bmi")
+    # if we want we can add END dummy node similarly
+graph.add_edge("calculate_bmi", END)
+
+graph_compiled = graph.compile()
+
+height = float(input("enter the height in mtr : "))
+weight = float(input("enter the weight in Kg : "))
+response = graph_compiled.invoke({"height":height, "weight":weight})
+print("BMI :", response['bmi'])
+
